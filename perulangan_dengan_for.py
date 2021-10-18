3fac2d151d547028328f65f100e0852c31dfa414
mrazak <muhamadrozak9@gmail.com>
2021-10-18 10:50:24 +0700

    Perulangan dan while dengan jumlah tertentu

---
+++ perulangan_dengan_for.py
@@ -0,0 +1,11 @@
+jumlah_buku = 10
+
+print('Ibu: "Baca semua bukumu!"')
+
+jumlah_buku_sudah_dibaca = 0
+print(f"Jumlah buku yang harus di baca {jumlah_buku}")
+print(f"Jumlah buku yang sudah di baca {jumlah_buku_sudah_dibaca}")
+for jumlah_buku_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah di baca {jumlah_buku_sudah_dibaca}")
---
+++ perulangan_dengan_while.py
@@ -0,0 +1,14 @@
+jumlah_buku = 10
+
+print('Ibu: "Baca semua bukumu!"')
+
+jumlah_buku_sudah_dibaca = 0
+print(f"Jumlah buku yang harus di baca {jumlah_buku}")
+print(f"Jumlah buku yang sudah di baca {jumlah_buku_sudah_dibaca}")
+
+while jumlah_buku_sudah_dibaca < jumlah_buku:
+    jumlah_buku_sudah_dibaca = jumlah_buku_sudah_dibaca + 1
+    print(f"Buku ke {jumlah_buku_sudah_dibaca} Sudah dibaca")
+
+print(f"Jumlah buku yang sudah di baca {jumlah_buku_sudah_dibaca}")
+
