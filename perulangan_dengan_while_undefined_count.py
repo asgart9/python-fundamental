69864e43cc47a628bc003f3bc5f7036ba6dd5424
mrazak <muhamadrozak9@gmail.com>
2021-10-18 11:27:29 +0700

    minor revision

--- perulangan_dengan_while_undefined_count.py
+++ perulangan_dengan_while_undefined_count.py
@@ -2,22 +2,22 @@ jumlah_buku = 10
 
 print('Ibu: "Baca semua bukumu sampai paham!"')
 
-jumlah_buku_sudah_dibaca_dan_dipahami = 0
-total_jumlah_baca = 0
+Jumlah_baca_paham = 0
+jumlah_baca = 0
 print(f"Jumlah buku yang harus di baca {jumlah_buku}")
-print(f"Jumlah buku yang sudah di baca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}")
+print(f"Jumlah buku yang sudah di baca dan dipahami {Jumlah_baca_paham}")
 
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_sudah_dibaca_dan_dipahami == 3:
-        print(f"Buku ke {jumlah_buku_sudah_dibaca_dan_dipahami+1} belum paham")
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if Jumlah_baca_paham == 3:
+        print(f"Buku ke {Jumlah_baca_paham + 1} belum paham")
     else:
-        jumlah_buku_sudah_dibaca_dan_dipahami = jumlah_buku_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} Sudah dibaca dan dipahami")
+        Jumlah_baca_paham = Jumlah_baca_paham + 1
+        print(f"Buku ke {Jumlah_baca_paham} Sudah dibaca dan dipahami")
 
 
-print(f"Jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca_dan_dipahami+1} dan yang dipahami {jumlah_buku_sudah_dibaca_dan_dipahami} buku")
-if jumlah_buku_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f"Jumlah buku yang sudah dibaca {Jumlah_baca_paham + 1} dan yang dipahami {Jumlah_baca_paham} buku")
+if Jumlah_baca_paham == jumlah_buku:
     print('"Bu, semua buku sudah dibaca dan dipahami')
 else:
-    print(f'"Bu, Tidak semua buku bisa di pahami, budi hanya paham {jumlah_buku_sudah_dibaca_dan_dipahami} buku')
+    print(f'"Bu, Tidak semua buku bisa di pahami, budi hanya paham {Jumlah_baca_paham} buku')
